c6a8a8f69bdd62846f8aa7fba901761ccbe9a3c3
Tobert03 <tobias.72518@protonmail.com>
2026-01-10 14:00:30 +0100

    recording function neu angefangen, erkennt jz wenn audio datei schon existiert und appended anstatt imme rzu überschreiben

--- recording.py
+++ recording.py
@@ -1,43 +1,52 @@
 import tkinter as tk
-import pyperclip
-import speech_recognition as sr
-import threading
+import pyaudio, sys, tiktoken, wave
+from pathlib import Path
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 if sys.platform == 'darwin' else 2
+RATE = 44100
+RECORD_SECONDS = 5
+
+AUDIO_FILE = 'recording.wav'
+FILE_PATH = Path(AUDIO_FILE)
 
 def recording():
-  global listening, stopper, rec_button
 
-  if not listening:
+  #recording_mode = 'ab' if FILE_PATH.exists() else 'wb'
 
-    listening = True
-    rec_button.config(text= "Aufnahme Beenden")
+  # if the recording file already existst we safe the old frames, so we can write them into the new file as well
+  if FILE_PATH.exists():
+     with wave.open(AUDIO_FILE, "rb") as r:
+      params = r.getparams()
+      old_frames = r.readframes(r.getnframes())
 
-    mic = sr.Microphone()
-    r = sr.Recognizer()
-    r.pause_threshold = 20
+  # we open the .wav file in wb= write binary mode (there is no append mode for wave), we first write the old_frames to the file and then record new audio
+  with wave.open(AUDIO_FILE, 'wb') as wf:
+      p = pyaudio.PyAudio()
+      wf.setnchannels(CHANNELS)
+      wf.setsampwidth(p.get_sample_size(FORMAT))
+      wf.setframerate(RATE)
+      wf.writeframes(old_frames)
 
-    def callback(r, audio):
-      global audio_data
-      audio_data = audio
-    
-    stopper = r.listen_in_background(mic, callback)
+      stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
 
-    return
+      # recording audio
+      for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        wf.writeframes(stream.read(CHUNK))
 
-  if listening:
 
-    listening = False
-    rec_button.config(text = "Aufnahme Starten")
-
-    if stopper:
-      stopper()
-      stopper = None
+      '''
+      print('Recording...')
+      for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+          wf.writeframes(stream.read(CHUNK))
+      print('Done')
+      '''
+      
+      stream.close()
+      p.terminate()
 
 def main():
-
-  global listening 
-  listening = False
-  global stopper
-  stopper = None
 
   root = tk.Tk()
   root.title("Speech to Text")
